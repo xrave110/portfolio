0f6458473288a68ed49259e834d868b0b74c778c
xRave110 <xRave110@protonmail.com>
2022-07-23 22:12:13 +0200

    Added img and fixed desc

--- src/components/blocks/Info.js
+++ src/components/blocks/Info.js
@@ -1,23 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import "./Info.css";
+import avatar from "../../imgs/avatar1.jpg";
 
 const Info = ({ currentState }) => {
     return (
         <div className="info-container">
             <h3>{currentState}</h3>
-            {/* <img src={logo} alt="" style={{ width: "98px", height: "98px" }} /> */}
-            <svg className="bd-placeholder-img rounded-circle img-container" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
+            <img src={avatar} className="bd-placeholder-img rounded-circle img-container" width="170" height="182" />
             <div className="about-container">
                 <h6 className='badge bg-info rounded-pill'>Automotive experience</h6>
                 <p className="info-paragraph">My name is Kamil and I am Software Engineer with 5 years experience in automotive industry.
                     During my career I have managed to develop and test multiple Software Components using C and Python. I was strongly involved in unit and integration
-                    testing for CAN messages. At the end of my automotive journey I have become Software Integrator. It allows me to focus more on integration topics (integrations tests, scripting, builds).
+                    testing for CAN messages. At the end of my automotive journey I have become Software Integrator. It allowed me to focus more on integration topics (integrations tests, scripting, builds).
                 </p>
                 <h6 className='badge bg-success rounded-pill'>Blockchain experience</h6>
                 <p className="info-paragraph">Since 2019 I have got interested in blockchain technology, especially in DeFi.
                     I have started using DeFi protocols and was amazed how it works without any intermediares - just a math and peers.
-                    I have been taking part in Moralis academy for a year and then continue my blockchain adventure with my own projects.
+                    I have been taking part in Moralis academy for a year and then continued my blockchain adventure with my own projects.
                     Besides <a href='https://academy.moralis.io/' target="_blank" rel="noreferrer" className='a-color'>Moralis Academy</a> my best places to learn were i.a:
                     <ul>
                         <li><a href='https://www.youtube.com/c/PatrickCollins' target="_blank" rel="noreferrer" className='a-color'>PatrickColins</a></li>
